28397836e4453cdd0b0b44a4e045665999f9b7c0
Vasant Saladi <60614334+vasantsaladi@users.noreply.github.com>
2023-06-17 18:06:17 -0400

    Update Weather.js

--- src/Components/Home/Weather.js
+++ src/Components/Home/Weather.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 async function getWeather(latitude, longitude) {
   try {
-    const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
+    const response = await axios.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m');
     const weatherData = response.data;
     const hourlyTemperature = weatherData.hourly.temperature_2m;
     return hourlyTemperature;
