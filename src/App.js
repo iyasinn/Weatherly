b142e0f44eca3c3909bae9a7ecef6e2556308c8a
nahianreza <91044737+nahianreza@users.noreply.github.com>
2023-06-18 01:19:40 -0400

    Fixed issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
-import Login from "./Components/Login/Login";
 import Home from "./Components/Home/Home";
 import Settings from "./Components/Settings/Settings";
 
 function App() {
-	const [page, setPage] = useState("Login");
+	const [page, setPage] = useState("Home");
 
 	const renderPage = (page) => {
 		return <Home></Home>;
