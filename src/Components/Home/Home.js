d0911d333b5262b2be08978ca68db29768f952df
Vasant Saladi <60614334+vasantsaladi@users.noreply.github.com>
2023-06-18 02:43:36 -0400

    weather is working

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,54 +1,47 @@
-import * as React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Search from "./Search";
 import NewsList from "./NewsList";
+import Weather from "./Weather";
 
 async function fetchGeocode(placeId, apiKey) {
-	const url = `https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=${apiKey}`;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?place_id=${placeId}&key=${apiKey}`;
 
-	try {
-		const response = await fetch(url);
-		if (response.ok) {
-			const data = await response.json();
-			return data;
-		} else {
-			throw new Error("Request failed with status:", response.status);
-		}
-	} catch (error) {
-		console.error("Error fetching geocode:", error);
-		return null;
-	}
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    } else {
+      throw new Error("Request failed with status:", response.status);
+    }
+  } catch (error) {
+    console.error("Error fetching geocode:", error);
+    return null;
+  }
 }
 
 const Home = () => {
-	const [location, setLocation] = React.useState(null);
-	const [data, setData] = React.useState(null);
-	const [city, setCity] = React.useState({});
-	const [foundCity, setFoundCity] = React.useState(false);
+  const [location, setLocation] = React.useState(null);
+  const [data, setData] = React.useState(null);
+  const [city, setCity] = React.useState({});
+  const [foundCity, setFoundCity] = React.useState(false);
 
-	useEffect(() => {
-		const apiKey = "AIzaSyAC0Sehx-7YO-Gx4AzBPdfzsuRwVNmU150";
-		// console.log("Update");
+  useEffect(() => {
+    const apiKey = "AIzaSyAC0Sehx-7YO-Gx4AzBPdfzsuRwVNmU150";
 
-		if (!location) {
-			return;
-		}
+    if (!location) {
+      return;
+    }
 
-		fetchGeocode(location.place_id, apiKey)
-			.then((rData) => {
-				// Handle the geocode response data
-				setData(rData);
-				setFoundCity(true);
+    fetchGeocode(location.place_id, apiKey)
+      .then((rData) => {
+        setData(rData);
+        setFoundCity(true);
 
-				console.log("Data", rData);
-				console.log("Data location", rData.results[0].geometry.location);
-				const address = rData.results[0].formatted_address;
-				const lat = rData.results[0].geometry.location.lat;
-				const long = rData.results[0].geometry.location.lng;
-				
-				const addressComponents = rData.results[0].address_components;
-				let cityName;
+        console.log("Data", rData);
+        console.log("Data location", rData.results[0].geometry.location);
 
+<<<<<<< Updated upstream
 				for (let i = 0; i < addressComponents.length; i++) {
 					if (addressComponents[i].types.includes("locality")) {
 						cityName = addressComponents[i].long_name;
@@ -64,17 +57,43 @@ const Home = () => {
 				console.error(error);
 			});
 	}, [location]);
+=======
+        const address = rData.results[0].formatted_address;
+        const lat = rData.results[0].geometry.location.lat;
+        const long = rData.results[0].geometry.location.lng;
+>>>>>>> Stashed changes
 
-	return (
-		<>
-			<h1 className="text-center font-bold text-3xl text-sky-400">
-				Home
-			</h1>
-			<Search location={setLocation}></Search>
-			{city.name && <NewsList cityName={city.name} />}
+        const addressComponents = rData.results[0].address_components;
+        let cityName;
 
-		</>
-	);
+        for (let i = 0; i < addressComponents.length; i++) {
+          if (addressComponents[i].types.includes("locality")) {
+            cityName = addressComponents[i].long_name;
+            break;
+          }
+        }
+
+        setCity({ address, lat, long, name: cityName });
+      })
+      .catch((error) => {
+        setFoundCity(false);
+        console.error(error);
+      });
+  }, [location]);
+
+  return (
+    <>
+      <h1 className="text-center font-bold text-3xl text-sky-400">Home</h1>
+      <Search location={setLocation} />
+      {city.name && (
+        <>
+          <Weather latitude={city.lat} longitude={city.long} />
+          <NewsList cityName={city.name} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Home;
+
--- src/Components/Home/Weather.js
+++ src/Components/Home/Weather.js
@@ -1,19 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+const Weather = ({ latitude, longitude }) => {
+  const [temperature, setTemperature] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-const axios = require('axios');
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m`
+        );
+        setTemperature(response.data.hourly.temperature_2m);
+        setLoading(false);
+      } catch (error) {
+        setError("Failed to fetch weather data");
+        setLoading(false);
+      }
+    };
 
-async function getWeather(latitude, longitude) {
-  try {
-    const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`);
-    const weatherData = response.data;
-    const hourlyTemperature = weatherData.hourly.temperature_2m;
-    return hourlyTemperature;
-  } catch (error) {
-    console.error('Error retrieving weather:', error);
-    throw error;
+    fetchWeather();
+  }, [latitude, longitude]);
+
+  if (loading) {
+    return <div>Loading weather...</div>;
   }
-}
 
-module.exports = {
-  getWeather
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div>
+      <h2>Weather</h2>
+      <p>Temperature: {temperature}°C</p>
+    </div>
+  );
 };
+
+export default Weather;
